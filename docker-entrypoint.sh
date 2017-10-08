14189a18bdfcca4d116816a73d61da5e4fdde3e4
klunejko <konrad@klunejko.net>
2017-10-08 22:49:51 +0200

    Update docker-entrypoint.sh
    fix cache

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,34 @@
 #!/bin/sh
 # ----------------------------------------------------------------------------
 # entrypoint for squid container
+# with changes from sameersbn/docker-squid to configure the cache
 # ----------------------------------------------------------------------------
 set -e
 
 SQUID_VERSION=$(/usr/sbin/squid -v | grep Version | awk '{ print $4 }')
-if [ "$1" == "squid" ]; then
-  echo "Staring squid [${SQUID_VERSION}]"
+
+create_log_dir() {
+  mkdir -p /var/log/squid
+  chmod -R 755 /var/log/squid
+  chown -R squid:squid /var/log/squid
+}
+
+create_cache_dir() {
+  mkdir -p /var/cache/squid
   chown -R squid:squid /var/cache/squid
+}
+
+create_log_dir
+create_cache_dir
+
+# default behaviour is to launch squid
+if [ "$1" == "squid" ]; then
+  if [[ ! -d /var/cache/squid/00 ]]; then
+    echo "Initializing cache..."
+    /usr/sbin/squid -N -f /etc/squid/squid.conf -z
+  fi
+  echo "Starting squid [${SQUID_VERSION}]"
   exec /sbin/su-exec root /usr/sbin/squid -N
 else
   exec "$@"
 fi
-
